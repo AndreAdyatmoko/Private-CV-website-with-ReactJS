21bd0f9090341c0b5c1b1f1d0d5f9a464db77aba
AndreAdyatmoko <AndreAdyatmoko>
2023-10-24 10:17:49 +0700

    add new resuma

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import './about.css';
-import ME from '../../assets/PP.jpg';
-import { FiAward } from 'react-icons/fi';
-import { MdGpsFixed } from 'react-icons/md';
-import { BiShoppingBag } from 'react-icons/bi';
+import React from "react";
+import "./about.css";
+import ME from "../../assets/PP.jpg";
+import { FiAward } from "react-icons/fi";
+import { MdGpsFixed } from "react-icons/md";
+import { BiShoppingBag } from "react-icons/bi";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
-      <div className='container about_container'>
-        <div className='about_me'>
-          <div className='about_me-image'>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
             <img src={ME} alt="me" />
           </div>
         </div>
 
-        <div className='about_content'>
-          <div className='about_cards'>
-            <article className='about_card'>
-              <FiAward className='about_icon' />
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FiAward className="about_icon" />
               <h5>Experience</h5>
-              <small>I don't have direct work experience</small>
+              <small>Work as Backend Developer in Redhunter</small>
             </article>
 
-            <article className='about_card'>
-              <MdGpsFixed className='about_icon' />
+            <article className="about_card">
+              <MdGpsFixed className="about_icon" />
               <h5>Skill</h5>
               <small>JavaScript</small>
               <br />
@@ -40,8 +40,8 @@ const About = () => {
               <small>MySQL</small>
             </article>
 
-            <article className='about_card'>
-              <BiShoppingBag className='about_icon' />
+            <article className="about_card">
+              <BiShoppingBag className="about_icon" />
               <h5>Project</h5>
               <small>1. Build FrontEnd Blog Apps</small>
               <br />
@@ -50,13 +50,18 @@ const About = () => {
               <small>3. Build FullStack Cashier App</small>
             </article>
           </div>
-          <p>Above are examples of some of the projects I made during my training period, for more details I will attach them on the next page!</p>
+          <p>
+            Above are examples of some of the projects I made during my training
+            period, for more details I will attach them on the next page!
+          </p>
 
-          <a href="#contact" className='btn btn-primary'>Talk about it</a>
+          <a href="#contact" className="btn btn-primary">
+            Talk about it
+          </a>
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default About;
